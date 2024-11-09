e2df9395715c7fdf3d56a99bbb304948d1d8973c
muthulakshmitheniah <130384981+muthulakshmitheniah@users.noreply.github.com>
2024-11-09 14:33:01 +0530

    Add files via upload

---
+++ Fibo.java
@@ -0,0 +1,16 @@
+public class Fibo{
+    public static void main(String[] args){
+        int count =0;
+        System.out.Println("Fibonacci series: ")
+        for(int i=0;i<count;i++){
+            System.out.Print(fibonacci(i)+" ")
+        }
+    }
+
+    public static int fibonacci(int i){
+        if (i<=0){
+            return 1;
+        }
+        return fibonacci(i-1)+fibonacci(i-2)
+    }
+}
